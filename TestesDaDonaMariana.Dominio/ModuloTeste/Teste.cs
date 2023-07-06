12cc5a80a3787851787ae18e1ea33e0ebb2de06e
Jesiane Straubel <jesi1951@hotmail.com>
2023-07-06 07:19:20 -0300

    Dominio Teste.

--- TestesDaDonaMariana.Dominio/ModuloTeste/Teste.cs
+++ TestesDaDonaMariana.Dominio/ModuloTeste/Teste.cs
@@ -1,22 +1,40 @@
 ﻿using TestesDaDonaMariana.Dominio.Compartilhado;
+using TestesDaDonaMariana.Dominio.ModuloMateria;
 
 namespace TestesDaDonaMariana.Dominio.ModuloTeste
 {
     public class Teste : EntidadeBase<Teste>
     {
+        public string titulo { get; set; }
+        public int numQuestoes { get; set; }
+        public Materia materia { get; set; }
+        public DateTime dataCriacao { get; set; }
         public Teste()
         {
             
         }
+        public Teste(int id, Materia materia, int numQuestoes, DateTime datacriacao)
+        {
+            this.id = id;
+            this.materia = materia;
+            this.numQuestoes = numQuestoes;
+            this.dataCriacao = dataCriacao;
+        }
 
         public override void AtualizarInformacoes(Teste registroAtualizado)
         {
-            throw new NotImplementedException();
+            materia = registroAtualizado.materia;
+
         }
 
         public override string Validar()
         {
-            throw new NotImplementedException();
+            Validador valida = new();
+
+            if (materia == null)
+                return $"Você deve selecionar uma materia!";
+
+            return "";
         }
     }
 }
--- TestesDaDonaMariana.WinApp/ModuloTeste/TabelaTesteControl.cs
+++ TestesDaDonaMariana.WinApp/ModuloTeste/TabelaTesteControl.cs
@@ -1,10 +1,45 @@
-﻿namespace TestesDaDonaMariana.WinApp.ModuloTeste
+﻿using TestesDaDonaMariana.Dominio.ModuloMateria;
+using TestesDaDonaMariana.Dominio.ModuloTeste;
+
+namespace TestesDaDonaMariana.WinApp.ModuloTeste
 {
     public partial class TabelaTesteControl : UserControl
     {
         public TabelaTesteControl()
         {
             InitializeComponent();
+
+            grid.ConfigurarGridZebrado();
+            grid.ConfigurarGridSomenteLeitura();
+            grid.Columns.AddRange(ObterColunas());
+        }
+
+        private DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Enunciado", HeaderText = "Enunciado"},
+
+            };
+
+            return colunas;
+        }
+
+        public int ObterNumeroItemSelecionado()
+        {
+            return grid.SelecionarNumero<int>();
+        }
+
+        public void AtualizarRegistros(List<Teste> testes)
+        {
+            grid.Rows.Clear();
+
+            foreach (var teste in testes)
+            {
+                grid.Rows.Add();
+            }
         }
     }
 }
